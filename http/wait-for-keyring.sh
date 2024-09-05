937237e60b4235a5ee61e29a45aafe5b0188d4de
Manuel Gugger <manuel.gugger@gmail.com>
2024-09-05 18:29:52 +0200

    ...

--- http/wait-for-keyring.sh
+++
@@ -1,6 +0,0 @@
-echo waiting for pacman keyring init to be done
-
-while ! systemctl show pacman-init.service | grep SubState=exited; do
-    systemctl --no-pager status -n0 pacman-init.service || true
-    sleep 1
-done
